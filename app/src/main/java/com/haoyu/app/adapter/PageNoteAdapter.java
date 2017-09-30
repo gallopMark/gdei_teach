b0240c1b0a839f531b9a685f767746ab48ae46f4
thebounaryforever <411747165@qq.com>
2017-09-30 17:02:50 +0800

    commit

--- app/src/main/java/com/haoyu/app/adapter/PageNoteAdapter.java
+++ app/src/main/java/com/haoyu/app/adapter/PageNoteAdapter.java
@@ -17,7 +17,6 @@ import java.util.List;
  * 作者:马飞奔 Administrator
  */
 public class PageNoteAdapter extends BaseArrayRecyclerAdapter<NoteEntity> {
-
     public PageNoteAdapter(List<NoteEntity> dataList) {
         super(dataList);
     }
