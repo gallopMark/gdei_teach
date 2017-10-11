62fa147d8c68d8dc050f5623dc78b5e02d44a3bc
gallopMark <xiaoma920406@126.com>
2017-10-11 15:37:14 +0800

    Failed commit: Default (4)

--- app/src/main/java/com/haoyu/app/adapter/MoreSurveAdapter.java
+++ app/src/main/java/com/haoyu/app/adapter/MoreSurveAdapter.java
@@ -6,8 +6,8 @@ import android.widget.TextView;
 
 import com.haoyu.app.basehelper.BaseArrayRecyclerAdapter;
 import com.haoyu.app.entity.SurveyAnswerSubmission;
+import com.haoyu.app.gdei.teacher.R;
 import com.haoyu.app.imageloader.GlideImgManager;
-import com.haoyu.app.lego.teach.R;
 
 import java.util.List;
 
